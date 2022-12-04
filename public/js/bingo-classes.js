bf1839ba65f3550b390b739cd0b32130ac5b0eae
brockbritton <brockbritton@outlook.com>
2022-12-03 21:11:16 -0500

    player instructions and dropdowns

--- public/js/bingo-classes.js
+++ public/js/bingo-classes.js
@@ -2,8 +2,11 @@
 class CallerOrganizer {
     constructor() {
         this.caller = null;
-        this.buildCalledNumbersTable()
-
+        this.resetUIVisuals();
+        let pot_table_cells = document.getElementsByClassName("cell_number_class");
+        if (pot_table_cells.length == 0) {
+            this.buildCalledNumbersTable()
+        }
     }
 
     buildCalledNumbersTable() {
@@ -32,19 +35,82 @@ class CallerOrganizer {
         table_div.appendChild(table)
     
     }
-    
-
-    buildNewCaller(newCaller) {
-        this.resetUIVisuals()
-        this.caller = newCaller
-    }
 
     resetUIVisuals() {
         //reset the slider and checkboxes - so a new game cannot be accidentally created
+        const slider = document.getElementById("slider-checkbox")
+        if (slider.checked) {
+            slider.checked = false
+        }
+        const checkboxes = document.getElementsByClassName("select-column-checkbox");
+        for (let i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].checked) {
+                checkboxes[i].checked = false
+            }
+        }
+        
         //clear the called number text
+        let html_display = document.getElementById("html-call-display")
+        html_display.innerHTML = "&#8203;"
+        
         //reset the called numbers table
+        const table_cells = document.getElementsByClassName("cell_number_class")
+        for (let i = 0; i < table_cells.length; i++) {
+            if (table_cells[i].classList.length > 2) {
+                table_cells[i].classList.toggle("cell_number_called")
+            }
+        }
+    }
+    
+
+    buildNewCaller() {
+        const game_columns = this.collectColumns();
+        if (game_columns.length > 0) {
+            this.caller = new BingoCaller(game_columns)
+        } else {
+            alert("please select at least one column or toggle to all")
+        }
+        this.resetUIVisuals()
+    
     }
 
+    collectColumns() {
+        const checkboxes = document.getElementsByClassName("select-column-checkbox");
+        const slider = document.getElementById("slider-checkbox")
+        if (slider.checked) {
+            return [0, 1, 2, 3, 4];
+        } else {
+            let values = []; 
+            for (let i = 0; i < checkboxes.length; i++) {
+                if (checkboxes[i].checked) {
+                    values.push(Number(checkboxes[i].value))
+                }
+            }
+            return values
+        }
+    }
+
+    checkCallNumber() {
+        if (this.caller != null) {
+            this.caller.callNumber()
+        } else {
+            alert("please start a new caller game first")
+            let html_display = document.getElementById("html-call-display")
+            html_display.innerHTML = "&#8203;" 
+        }
+    }
+
+    toggleColumnsSelect() {
+        const slider = document.getElementById("slider-checkbox")
+        const checkboxes = document.getElementsByClassName("select-column-checkbox");
+        for (let i = 0; i < checkboxes.length; i++) {
+            if (slider.checked) {
+                checkboxes[i].checked = true;
+            } else {
+                checkboxes[i].checked = false;
+            }
+        }
+    }
 }
 
 class BingoCaller {
@@ -52,7 +118,6 @@ class BingoCaller {
         this.finished = false;
         this.past_calls = [];
         this.buildCallPool(column_values)
-
     }
 
     
@@ -66,7 +131,7 @@ class BingoCaller {
         }
     }
 
-    call_number() {
+    callNumber() {
         if (this.call_pool.length > 0) {
             let random_index = Math.floor(Math.random() * (this.call_pool.length));
             let called_num = this.call_pool[random_index]
@@ -78,9 +143,11 @@ class BingoCaller {
 
             const letters = ["B", "I", "N", "G", "O"]
             let html_display = document.getElementById("html-call-display")
-            html_display.innerHTML = `${letters[Math.floor(called_num / 15)]} ${called_num}`
+            html_display.innerHTML = `${letters[Math.floor(called_num / 15.1)]} ${called_num}`
         } else {
             alert("all numbers for this game have been called")
+            let html_display = document.getElementById("html-call-display")
+            html_display.innerHTML = "&#8203;"
         }
         
 
--- public/js/bingo.js
+++ public/js/bingo.js
@@ -1,56 +1,17 @@
 
 function switchBingoStates() {
+    let caller_div = document.getElementById("game-caller-div")
+    let player_div = document.getElementById("game-player-div")
     let dropdown = document.getElementById("choose-bingo-gamemode-drpdwn");
     if (dropdown.value == 0) {
         console.log("new game state: caller")
+        caller_div.style.display = "flex";
+        player_div.style.display = "none";
         curr_bingo_state = new CallerOrganizer();
     } else if (dropdown.value == 1) {
         console.log("new game state: player")
+        caller_div.style.display = "none";
+        player_div.style.display = "flex";
     }
 }
 
-function build_new_caller() {
-    const game_columns = collectColumns();
-    if (game_columns.length > 0) {
-        curr_bingo_state.buildNewCaller(new BingoCaller(game_columns))
-    } else {
-        alert("please select at least one column or toggle to all")
-    }
-}
-
-function caller_call_num() {
-    if (curr_bingo_state.caller != null) {
-        curr_bingo_state.caller.call_number()
-    } else {
-        alert("please start a new caller game first")
-    }
-}
-
-function collectColumns() {
-    const checkboxes = document.getElementsByClassName("select-column-checkbox");
-    const slider = document.getElementById("slider-checkbox")
-    if (slider.checked) {
-        return [0, 1, 2, 3, 4];
-    } else {
-        let values = []; 
-        for (let i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].checked) {
-                values.push(Number(checkboxes[i].value))
-            }
-        }
-        return values
-    }
-    
-}
-
-function toggle_columns_select() {
-    const slider = document.getElementById("slider-checkbox")
-    const checkboxes = document.getElementsByClassName("select-column-checkbox");
-    for (let i = 0; i < checkboxes.length; i++) {
-        if (slider.checked) {
-            checkboxes[i].checked = true;
-        } else {
-            checkboxes[i].checked = false;
-        }
-    }
-}
